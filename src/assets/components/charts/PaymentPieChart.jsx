181bd309b3dc5bc856eba36639af62676fd580cd
Himanshu Dutt <37894863+MyLordHitsHard@users.noreply.github.com>
2024-02-09 17:02:49 +0530

    Added responsiveness for mobile screens

--- src/assets/components/charts/PaymentPieChart.jsx
+++ src/assets/components/charts/PaymentPieChart.jsx
@@ -14,9 +14,14 @@ function PaymentPieChart({loanDetails, loanOutput}) {
                         loanOutput.reduce((acc, curr) => acc + parseFloat(curr.monthlyPayment), 0).toFixed(2)
                     ],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.6)',
-                    'rgba(54, 162, 235, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(255, 99, 132, 0.8)',
+                    'rgba(54, 162, 235, 0.8)',
+                    'rgba(255, 206, 86, 0.8)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
                 ],
             },
         ],
